54151c9f2651f0fb32cc662772991ce5658fd833
LeN <vladlenfrog@gmail.com>
2025-11-15 01:43:54 +0300

    next zone area

---
+++ Assets/_Main/Scripts/Location/NextAreaNetworkBehaviour.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using FishNet.Object;
+using UnityEngine;
+
+namespace _Main.Scripts.Location
+{
+	public class NextAreaNetworkBehaviour : NetworkBehaviour
+	{
+		public event Action<int> OnPlayerEntered;
+		public event Action<int> OnPlayerExited;
+		private void OnTriggerEnter(Collider other)
+		{
+			if (!NetworkManager.ServerManager.Started)
+			{
+				return;
+			}
+			
+			var player = other.GetComponent<NetworkObject>();
+			if (player == null)
+			{
+				return;
+			}
+			
+			OnPlayerEntered?.Invoke(player.OwnerId);
+		}
+
+		private void OnTriggerExit(Collider other)
+		{
+			if (!NetworkManager.ServerManager.Started)
+			{
+				return;
+			}
+			
+			var player = other.GetComponent<NetworkObject>();
+			if (player == null)
+			{
+				return;
+			}
+			
+			OnPlayerExited?.Invoke(player.OwnerId);
+		}
+	}
+}
---
+++ Assets/_Main/Scripts/Player/Network/NetworkPlayerNextAreaController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+using _Main.Scripts.Core;
+using _Main.Scripts.Location;
+using PlatformCore.Core;
+using PlatformCore.Infrastructure.Lifecycle;
+
+namespace _Main.Scripts.Player.Network
+{
+	public class NetworkPlayerNextAreaController : IBaseController, IActivatable, IUpdatable
+	{
+		private readonly NextAreaNetworkBehaviour _nextAreaNetworkBehaviour;
+		private readonly INetworkService _networkService;
+		
+		private HashSet<int> _playerIds = new HashSet<int>();
+		
+		public NetworkPlayerNextAreaController(NextAreaNetworkBehaviour nextAreaNetworkBehaviour, INetworkService networkService)
+		{
+			_nextAreaNetworkBehaviour = nextAreaNetworkBehaviour;
+			_networkService = networkService;
+		}
+		
+		public void Activate()
+		{
+			_nextAreaNetworkBehaviour.OnPlayerEntered += OnPlayerEnteredHandler;
+			_nextAreaNetworkBehaviour.OnPlayerExited += OnPlayerExitedHandler;
+		}
+
+		public void Deactivate()
+		{
+			_nextAreaNetworkBehaviour.OnPlayerEntered -= OnPlayerEnteredHandler;
+			_nextAreaNetworkBehaviour.OnPlayerExited -= OnPlayerExitedHandler;
+		}
+
+		public void OnUpdate(float deltaTime)
+		{
+			if (_playerIds.Count == 0)
+			{
+				return;
+			}
+
+			if (_playerIds.Count == _networkService.PlayersCount)
+			{
+				
+			}
+		}
+
+		private void OnPlayerEnteredHandler(int playerId)
+		{
+			
+		}
+
+		private void OnPlayerExitedHandler(int playerId)
+		{
+			
+		}
+		
+	}
+}
